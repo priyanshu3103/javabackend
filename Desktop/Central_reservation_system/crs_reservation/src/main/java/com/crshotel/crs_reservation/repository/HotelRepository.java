45fdd7fd8cec17e075b51c32417b05e240547e9f
Priyanshu Barapatre <101656280+priyanshu3103@users.noreply.github.com>
2025-05-11 14:49:28 +0530

    Delete Desktop/Central_reservation_system/crs_reservation directory

--- Desktop/Central_reservation_system/crs_reservation/src/main/java/com/crshotel/crs_reservation/repository/HotelRepository.java
+++
@@ -1,8 +0,0 @@
-package com.crshotel.crs_reservation.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.crshotel.crs_reservation.model.Hotel;
-
-public interface HotelRepository extends JpaRepository<Hotel, Long> {
-}
